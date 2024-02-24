a8da58af02bd09ce739f3726a192716a33269f5b
fahizrahmank <fahizrahmank@gmail.com>
2024-02-24 16:24:54 +0530

    insta-clone

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Button } from '@chakra-ui/react'
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Button>gbthbiub</Button>
+    </>
+  )
+}
+
+export default App
